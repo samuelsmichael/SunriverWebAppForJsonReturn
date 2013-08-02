250848749698e9913b8ed5a61128d4f8bb944cee
Michael Samuels <samuelsmichael222@gmail.com>
2013-08-02 00:43:09 -0600

    Initial move from RNS

--- src/com/sdouglas/android/commuteralert/LocationService.java
+++ src/com/sdouglas/android/commuteralert/LocationService.java
@@ -50,30 +50,28 @@ public class LocationService extends Service implements LocationListener  {
 		super.onStart(intent, startId);
 		Thread
 		.setDefaultUncaughtExceptionHandler(new CustomExceptionHandlerTimer(this));
-		if(intent != null) {
-			if(intent.getAction()=="JustDisarm") {
-		    	getNotificationManager().cancel(ARMED_NOTIFICATION_ID);	
-		    	stopMyLocationsTimer2();
+		if(intent!= null && intent.getAction()=="JustDisarm") {
+	    	getNotificationManager().cancel(ARMED_NOTIFICATION_ID);	
+	    	stopMyLocationsTimer2();
+		} else {
+			if(intent!=null && intent.getAction()=="JustInitializeLocationManager") {
+				initializeLocationManager();
 			} else {
-				if(intent.getAction()=="JustInitializeLocationManager") {
-					initializeLocationManager();
-				} else {
+				if (intent!=null) {
 					mAddressInReadableForm=intent.getStringExtra("LocationAddress");
 			    	Notification.Builder mBuilder=new Notification.Builder(this)
 				    	.setSmallIcon(R.drawable.ic_launcher)
 				    	.setContentTitle("CommuterAlert is armed")
 				    	.setContentText(mAddressInReadableForm)
 				    	.setOngoing(true);
-			    	
 			    	// Creates an explicit intent for an Activity in your app
 			    	Intent resultIntent = new Intent(this, LocationService.class);
 					PendingIntent pendingIntent = PendingIntent.getActivity(this,
 							(int)System.currentTimeMillis(), resultIntent, 0);
 			    	mBuilder.setContentIntent(pendingIntent);    	    	
 			    	getNotificationManager().notify(ARMED_NOTIFICATION_ID, mBuilder.getNotification());
-		
-					getmAlarmSender();
-				}
+				}	
+				getmAlarmSender();
 			}
 		}
 	}
