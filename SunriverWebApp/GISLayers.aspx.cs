148949668b3b7f8a2dfb3a8d0b4663098ecb57fe
Mike Samuels <samuelsmichael222@gmail.com>
2014-05-29 05:58:08 -0600

    Created GISLayers function (so I could test)

---
+++ SunriverWebApp/GISLayers.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using Newtonsoft.Json;
+using System.Text;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Web.Services;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace SunriverWebApp {
+    public partial class GISLayers1 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            MemoryStream ms = new MemoryStream();
+            JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+            using (JsonTextWriter jsonTextWriter = new JsonTextWriter(
+                new StreamWriter(ms, new UTF8Encoding(false, true))) { CloseOutput = false }) {
+                serializer.Serialize(jsonTextWriter, GISLayers.Sample);// new GISLayers().buildList());
+                jsonTextWriter.Flush();
+            }
+            Utils.jsonSerializeStep2(ms, Response);
+
+        }
+    }
+}
---
+++ SunriverWebApp/GISLayers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+using System.Runtime.Serialization;
+
+namespace SunriverWebApp {
+    [DataContractAttribute]
+    public class GISLayers : WebServiceItem {
+        [DataMemberAttribute]
+        public int srGISLayersId { get; set; }
+        [DataMemberAttribute]
+        public int srGISLayersUseNum { get; set; }
+        [DataMemberAttribute]
+        public string srGISLayersURL { get; set; }
+        [DataMemberAttribute]
+        public bool srGISLayersIsBikePaths { get; set; }
+
+        protected override WebServiceItem objectFromDatasetRow(System.Data.DataRow dr) {
+            GISLayers gisLayers = new GISLayers();
+            gisLayers.srGISLayersId=Utils.ObjectToInt(dr["srGISLayersId"]);
+            gisLayers.srGISLayersIsBikePaths = Utils.ObjectToBool(dr["srGISLayersIsBikePaths"]);
+            gisLayers.srGISLayersURL = Utils.ObjectToString(dr["srGISLayersURL"]);
+            gisLayers.srGISLayersUseNum = Utils.ObjectToInt(dr["srGISLayersUseNum"]);
+            return gisLayers;
+        }
+
+        public static List<GISLayers> Sample {
+            get {
+                List<GISLayers> list = new List<GISLayers>();
+                list.Add(new GISLayers {
+                    srGISLayersId = 100,
+                    srGISLayersUseNum = 1,
+                    srGISLayersIsBikePaths=true,
+                    srGISLayersURL = "http://tiles.arcgis.com/tiles/PPpMbTaRDuKoF0e4/arcgis/rest/services/SRPathways/MapServer"
+                });
+                list.Add(new GISLayers {
+                    srGISLayersId = 200,
+                    srGISLayersUseNum=1,
+                    srGISLayersIsBikePaths=false,
+                    srGISLayersURL = "http://services.arcgisonline.com/ArcGIS/rest/services/USA_Topo_Maps/MapServer"
+                });
+                return list;
+            }
+        }
+
+        public List<GISLayers> buildList() {
+            List<GISLayers> list = new List<GISLayers>();
+            foreach (DataRow dr in getDataSet().Tables[0].Rows) {
+                list.Add((GISLayers)objectFromDatasetRow(dr));
+            }
+            return list;
+        }
+    }
+}
